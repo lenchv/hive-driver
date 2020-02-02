bf7e3471e6b058e5c953169f5d79985df69ea678
Volodymyr Liench <lenchvov@gmail.com>
2020-02-02 20:14:29 +0200

    add kerberized hive

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const HiveDriver = require('./dist/hive/HiveDriver').default;
 const NoSaslAuthentication = require('./dist/connection/auth/NoSaslAuthentication').default;
 const PlainTcpAuthentication = require('./dist/connection/auth/PlainTcpAuthentication').default;
 const PlainHttpAuthentication = require('./dist/connection/auth/PlainHttpAuthentication').default;
+const KerberosTcpAuthentication = require('./dist/connection/auth/KerberosTcpAuthentication').default;
 const HttpConnection = require('./dist/connection/connections/HttpConnection').default;
 const TcpConnection = require('./dist/connection/connections/TcpConnection').default;
 const HiveUtils = require('./dist/utils/HiveUtils').default;
@@ -15,6 +16,7 @@ module.exports = {
         NoSaslAuthentication,
         PlainTcpAuthentication,
         PlainHttpAuthentication,
+        KerberosTcpAuthentication,
     },
     connections: {
         HttpConnection,
