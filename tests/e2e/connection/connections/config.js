afba5ab3d182adf5c84c0f18c8ff3b1c7cc6e7dc
Volodymyr Liench <lenchvov@gmail.com>
2020-02-18 20:33:37 +0200

    ci: add loggig hostname

--- tests/e2e/connection/connections/config.js
+++ tests/e2e/connection/connections/config.js
@@ -1,9 +1,12 @@
 const fqdn = require('get-fqdn');
 const path = require('path');
 
-module.exports = () => fqdn().then(hostname => ({
-    hostname,
-    ca: path.resolve(__dirname, '../../../../.docker/ssl/ca.pem'),
-    cert: path.resolve(__dirname, '../../../../.docker/ssl/cert.pem'),
-    key: path.resolve(__dirname, '../../../../.docker/ssl/key.key'),
-}));
+module.exports = () => fqdn().then(hostname => {
+    console.log(hostname);
+    return ({
+        hostname,
+        ca: path.resolve(__dirname, '../../../../.docker/ssl/ca.pem'),
+        cert: path.resolve(__dirname, '../../../../.docker/ssl/cert.pem'),
+        key: path.resolve(__dirname, '../../../../.docker/ssl/key.key'),
+    });
+});
